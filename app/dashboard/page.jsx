1ed4ec5c8da46ae0263252f68644a300f6f9ad2b
Harsh verma <hv72002@gmail.com>
2024-07-27 19:55:22 +0530

    little changes

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,11 +1,8 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
-import Header from './_components/Header'
-
 function Dashboard() {
   return (
     <div>
-      <Header />
       page hi
       <UserButton />
     </div>
