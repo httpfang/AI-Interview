942c94439b8482973e846b9d945b4a6dd507663a
Harsh verma <hv72002@gmail.com>
2024-07-31 15:51:42 +0530

    Add textarea component for job description in GEMINIAI API_KEY AddNewInterview.jsx

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -15,33 +15,70 @@ import { Textarea } from '@/components/ui/textarea';
 
 function AddNewInterview() {
     const [openDialog, setOpenDialog] = useState(false);
+    const [jobPosition, setJobPosition] = useState()
+    const [jobDesc, setJobDesc] = useState()
+    const [jobExperience, setJobExpirience] = useState()
+    const onSubmit = (e) => {
+      e.preventDefault()
+      console.log("onSubmit");
+    }
   return (
     <div>
-      <div className="p-10border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all" onClick={()=>setOpenDialog(true)}>
+      <div
+        className="p-10border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all"
+        onClick={() => setOpenDialog(true)}
+      >
         <h2 className="font-bold text-lg text-center">+ Add New</h2>
       </div>
 
       <Dialog open={openDialog}>
-        
         <DialogContent className="max-w-xl">
           <DialogHeader>
-            <DialogTitle className="text-2xl">Tells Us More About Your Interviewing</DialogTitle>
+            <DialogTitle className="text-2xl">
+              Tells Us More About Your Interviewing
+            </DialogTitle>
             <DialogDescription>
-              <div>
-                <h2>Add details about your job position/role, job description and years of experience</h2>
-                <div className='mt-4 my-3'>
-                    <lebel >job role/job position</lebel>
-                    <Input className="mt-3" placeholder="Eg: frontend developer" />
+              <form onSubmit={onSubmit}>
+                <div>
+                  <h2>
+                    Add details about your job position/role, job description
+                    and years of experience
+                  </h2>
+                  <div className="mt-4 my-3">
+                    <lebel>Job role/Job Position</lebel>
+                    <Input
+                      className="mt-3"
+                      placeholder="Eg: frontend developer"
+                      required
+                      onChange={(e) => setJobPosition(e.target.value)}
+                    />
+                  </div>
+                  <div className="my-3">
+                    <lebel>Job description/ Tech stack (in short)</lebel>
+                    <Textarea
+                      className="mt-3"
+                      placeholder="Eg: React, next, angular"
+                      required
+                      onChange={(e) => setJobDesc(e.target.value)}
+                    />
+                  </div>
+                  <div className=" my-3">
+                    <lebel>Years of experience</lebel>
+                    <Input className="mt-3" placeholder="Eg.5" type="number" required onChange={(e) => setJobExpirience(e.target.value)}/>
+                  </div>
                 </div>
-                <div className='my-3'>
-                    <lebel >job description/ Tech stack (in short)</lebel>
-                    <Textarea className="mt-3" placeholder="Eg: React, next, angular" />
+                <div className="flex gap-5 justify-end">
+                  <Button
+                    type="button"
+                    className="border-none"
+                    variant="ghost"
+                    onClick={() => setOpenDialog(false)}
+                  >
+                    Cancel
+                  </Button>
+                  <Button type="submit">Start Interview</Button>
                 </div>
-              </div>
-              <div className='flex gap-5 justify-end'>
-                <Button className="border-none" variant = "ghost" onClick={()=> setOpenDialog(false)}>Cancel</Button>
-                <Button>Start Interview</Button>
-              </div>
+              </form>
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
